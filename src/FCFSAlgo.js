3ab82e0934237b95ad59badb106d4ee44f37ef08
Ahmed Sakr <80043395+Ahmed-Mohmed-Sakr@users.noreply.github.com>
2022-12-23 13:40:44 +0200

    FCFS Algo

--- src/FCFSAlgo.js
+++ src/FCFSAlgo.js
@@ -1,5 +1,54 @@
 import { filter } from "./filters";
 
-const getApriodicSchedule = (filterTaskes) => {};
+const FirstComeFirstServed = (comingTasks) =>
+  comingTasks.sort((a, b) => {
+    if (a.arrivalTime < b.arrivalTime) {
+      return -1;
+    } else if (a.arrivalTime > b.arrivalTime) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+const getApriodicSchedule = (filterTaskes) => {
+  const makeTasksReady = structuredClone(filterTaskes);
+  const messagesOfSchdulingProcess = [];
+  let processingTime = 0;
+  FirstComeFirstServed(makeTasksReady);
+
+  if (makeTasksReady.length > 0 && makeTasksReady[0].arrivalTime > 0) {
+    messagesOfSchdulingProcess.push(
+      `Time[${0}->${
+        makeTasksReady[0].arrivalTime
+      }]: Ideal Time there is no Tasks to process`
+    );
+    processingTime = makeTasksReady[0].arrivalTime;
+  }
+
+  makeTasksReady.forEach((task) => {
+    if (task.arrivalTime <= processingTime) {
+      const endingTime = processingTime + parseInt(task.executionTime);
+      if (endingTime <= parseInt(task.endingDeadline)) {
+        messagesOfSchdulingProcess.push(
+          `Time[${processingTime}->${endingTime}]: ${task.taskName}-(${task.id}) Finished successfully`
+        );
+        processingTime = endingTime;
+      } else {
+        messagesOfSchdulingProcess.push(
+          `Task: ${task.taskName}-(${task.id}) Faild`
+        );
+      }
+    } else {
+      const endingTime = processingTime + (task.arrivalTime - processingTime);
+      messagesOfSchdulingProcess.push(
+        `Time[${processingTime}->${endingTime}]: Ideal Time there is no Tasks to process`
+      );
+      processingTime = endingTime;
+    }
+  });
+
+  return messagesOfSchdulingProcess;
+};
 
 export { getApriodicSchedule };
--- src/scheduling.js
+++ src/scheduling.js
@@ -4,6 +4,10 @@ import { renderScheduling } from "./views";
 const typeOfTasks = location.hash.substring(1);
 const tasks = getTasks();
 
+if (typeOfTasks === "apriodic") {
+  document.querySelector("#steps-number").style.display = "none";
+}
+
 renderScheduling(typeOfTasks, tasks);
 
 document.querySelector("#steps-number").addEventListener("input", (e) => {
